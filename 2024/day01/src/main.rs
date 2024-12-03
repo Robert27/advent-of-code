f80f796afd3b5407068453462dd6dbb36726a586
Robert Eggl <github@roberteggl.dev>
2024-12-03 22:57:15 +0100

    feat: solves day 01.1 and 01.2

---
+++ 2024/day01/src/main.rs
@@ -0,0 +1,64 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+use std::collections::HashMap;
+
+fn load_input(file_path: &str) -> io::Result<(Vec<i32>, Vec<i32>)> {
+    let path = Path::new(file_path);
+    let file = File::open(&path)?;
+    let reader = io::BufReader::new(file);
+
+    let mut left_list = Vec::new();
+    let mut right_list = Vec::new();
+
+    for line in reader.lines() {
+        let line = line?;
+        let parts: Vec<&str> = line.split_whitespace().collect();
+        if parts.len() == 2 {
+            left_list.push(parts[0].parse::<i32>().unwrap());
+            right_list.push(parts[1].parse::<i32>().unwrap());
+        }
+    }
+
+    Ok((left_list, right_list))
+}
+
+fn part1() -> io::Result<()> {
+    let (mut left_list, mut right_list) = load_input("./input.txt")?;
+
+    left_list.sort();
+    right_list.sort();
+
+    let total_distance: i32 = left_list.iter()
+        .zip(right_list.iter())
+        .map(|(left, right)| (left - right).abs())
+        .sum();
+
+    println!("Part 1: {}", total_distance);
+
+    Ok(())
+}
+
+fn part2() -> io::Result<()> {
+    let (left_list, right_list) = load_input("./input.txt")?;
+
+    let right_count = right_list.iter().fold(HashMap::new(), |mut acc, &num| {
+        *acc.entry(num).or_insert(0) += 1;
+        acc
+    });
+
+    let total_distance: i32 = left_list.iter()
+        .map(|&left| left * right_count.get(&left).unwrap_or(&0))
+        .sum();
+
+    println!("Part 2: {}", total_distance);
+
+    Ok(())
+}
+
+fn main() -> io::Result<()> {
+    part1()?;
+    part2()?;
+
+    Ok(())
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# advent-of-code
+# Advent of Code 🎄
+
+Advent of Code is a series of small programming puzzles for a variety of skill levels.
+https://adventofcode.com
+
+This repository contains my solutions to the puzzles.
